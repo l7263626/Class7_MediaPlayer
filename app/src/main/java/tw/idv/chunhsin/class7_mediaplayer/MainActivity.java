a1470a676bdda0d3f3e620c4e7eedfcba1c57619
jasonlin <l7263626@msn.com>
2015-10-21 21:49:30 +0800

    靜態create

--- app/src/main/java/tw/idv/chunhsin/class7_mediaplayer/MainActivity.java
+++ app/src/main/java/tw/idv/chunhsin/class7_mediaplayer/MainActivity.java
@@ -1,17 +1,100 @@
 package tw.idv.chunhsin.class7_mediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button,button2,button3;
+    SeekBar seekBar;
+    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findviews();
     }
+
+    void findviews(){
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(onClickListener);
+        button2=(Button)findViewById(R.id.button2);
+        button2.setOnClickListener(onClickListener);
+        button3=(Button)findViewById(R.id.button3);
+        button3.setOnClickListener(onClickListener);
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.button:
+                    staticCall();
+                    break;
+                case R.id.button2:
+                    mp.pause();
+                    break;
+                case R.id.button3:
+                    if(mp.isPlaying()){
+                        mp.stop();
+                        mp.release();
+                        mp=null;
+                    }
+                    break;
+            }
+        }
+    };
+
+    void staticCall(){
+        if(mp==null){
+            mp = MediaPlayer.create(this,R.raw.ring);
+        }
+        if(!mp.isPlaying()){
+            mp.start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while(mp.isPlaying()){
+                        final int curPos = mp.getCurrentPosition();
+                        seekBar.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                seekBar.setScaleX(curPos);
+                            }
+                        });
+
+                    }
+                }
+            });
+        }
+    }
+
+    SeekBar.OnSeekBarChangeListener onSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBarr, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
